f9344d5aeb805d13aaa713753d8d726525a2640c
Mark Tristan C. Garcia <147889724+Tristan8100@users.noreply.github.com>
2026-01-21 15:23:54 +0800

    added redirect

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import type { Response } from 'express';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('redirect')
+  redirectToFrontend(@Res() res: Response) {
+    return res.redirect(process.env.FRONTEND_URL || 'http://localhost:3000');
   }
 }
